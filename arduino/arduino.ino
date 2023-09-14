8e03b6965945a4756805dd974e0981673082f8f9
Kevin Nikolai Mathisen <kevinnm@stud.ntnu.no>
2023-09-14 18:11:09 +0200

    feat: add support for 3 values on arduino

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -1,12 +1,17 @@
 //Constants:
-const int potPin = A0; //pin A0 to read analog input
+const int potPin0 = A0; //pin A0 to read analog input
+const int potPin1 = A1;
+const int potPin2 = A2;
 
 //Variables:
-int value; //save analog value
-
+int value1; //save analog value
+int value2;
+int value3;
 
 void setup(){
-  pinMode(potPin, INPUT); 
+  pinMode(potPin0, INPUT); 
+  pinMode(potPin1, INPUT); 
+  pinMode(potPin2, INPUT); 
 
   Serial.begin(9600);
 }
@@ -14,11 +19,13 @@ void setup(){
 void loop(){
   
   // Save analog value from potentiometer
-  value = analogRead(potPin);          
+  value1 = analogRead(potPin0);
+  value2 = analogRead(potPin1);
+  value3 = analogRead(potPin2);          
 
   // Send analog value to serial
-  Serial.println("^"+ (String)(value)+" 0$");
+  Serial.println("^"+ (String)(value1)+" "+ (String)(value2) + " " + (String)(value3) + "$");
 
-  delay(1000);                          
+  delay(1);                          
   
 }
